5430ffc896b60b1380ad641f389d3bd58b3358ee
ManuCiao <mn.sabatino@gmail.com>
2016-11-28 16:10:03 +0000

    stored the passed key and value in a file

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHeader(200, {"Content-Type": "text/plain"});
-  response.write("Hello World!!");
-  response.end();
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var port = 4000;
+var db = {};
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-server.listen(4000, function(){
-  console.log("Server is listening on port 4000");
+
+app.get('/set', function (req, res, next) {
+  var logs = [];
+  for (var key in req.query) {
+    db[key] = req.query[key];
+    console.log(key, req.query[key]);
+    logs.push('Data has been saved as: {' + key + ':' + req.query[key] + '}');
+  }
+  fs.writeFile('db.json', JSON.stringify(db), function (err)   {
+    if (err) return next(err);
+    res.send('Data successfully saved');
+    res.end(logs.join('\n'));
+  });
+});
+
+
+app.listen(port, function(err)  {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+  console.log(`server is listening on ${port}`);
 });
