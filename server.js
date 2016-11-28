b304da71655a21c41ae47b409abbc31510808075
ManuCiao <mn.sabatino@gmail.com>
2016-11-28 17:36:32 +0000

    return the value stored for the key

--- server.js
+++ server.js
@@ -5,28 +5,35 @@ var port = 4000;
 var db = {};
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    response.send('Hello World!');
 });
 
-
-app.get('/set', function (req, res, next) {
-  var logs = [];
-  for (var key in req.query) {
-    db[key] = req.query[key];
-    console.log(key, req.query[key]);
-    logs.push('Data has been saved as: {' + key + ':' + req.query[key] + '}');
-  }
-  fs.writeFile('db.json', JSON.stringify(db), function (err)   {
-    if (err) return next(err);
-    res.send('Data successfully saved');
-    res.end(logs.join('\n'));
-  });
+app.get('/set', function(req, res) {
+    var logs = [];
+    for (var key in req.query) {
+        db[key] = req.query[key];
+        logs.push('Data has been saved as: {' + key + ':' + req.query[key] + '}');
+    }
+    fs.writeFile('db.json', JSON.stringify(db), function(err) {
+        if (err) throw err;
+        res.send('Data successfully saved on the db.json file');
+        res.end(logs.join(','));
+    });
 });
 
+app.get('/get', function(req, res) {
+    fs.readFile('db.json', function(err, data) {
+        if (err) throw err;
+        json = JSON.parse(data);
+        for(var myKey in json) {
+          res.send("The value of " +myKey+ " is " +json[myKey]);
+        }
+    });
+});
 
-app.listen(port, function(err)  {
-  if (err) {
-    return console.log('something bad happened', err);
-  }
-  console.log(`server is listening on ${port}`);
+app.listen(port, function(err) {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+    console.log(`server is listening on ${port}`);
 });
